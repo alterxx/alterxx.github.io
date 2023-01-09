6a2163b334b767ac796b1616c351f17fd8c15c85
ALTERXX <108991552+alterxx@users.noreply.github.com>
2023-01-09 17:26:35 +0100

    Add files via upload

---
+++ home/app.js
@@ -0,0 +1,8 @@
+
+
+const love = {1:"HI! Welcome to my site's page! I hope you like it! :D", 2:"Love you! :3", 3:"Hope you find something good on this page"};
+const who = {1:"свою Батьківщину", 2:"самого себе", 3:"своїх родичів"};
+const mark = {1:".", 2:"!", 3:"?"};
+
+const i = 3;
+console.log();
